3894cbdb0427f67012e331495fcea17cf09ef33b
breadddevv <cor.joaocor@gmail.com>
2026-03-14 16:10:48 +0000

    a

--- components/newmembers.tsx
+++ components/newmembers.tsx
@@ -3,7 +3,6 @@ import { createPortal } from "react-dom";
 import { useRouter } from "next/router";
 import axios from "axios";
 import {
-  IconSparkles,
   IconPlayerPlay,
   IconPlayerPause,
   IconPencil,
