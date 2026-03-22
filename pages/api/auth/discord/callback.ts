cc82a92bda0e130e4cefe196f6a02b5dbdf6b9c9
Bread <tiagu.1789@gmail.com>
2026-03-22 20:53:08 +0000

    do u wanna like explain how u worked on dev env?

--- pages/api/auth/discord/callback.ts
+++ pages/api/auth/discord/callback.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { withSessionRoute } from '@/lib/withSession';
 import axios from 'axios';
 import Package from '@/package.json'
+import prisma from '@/utils/database';
 
 type DiscordTokenResponse = {
 	access_token: string;
