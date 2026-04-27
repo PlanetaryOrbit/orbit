6f915ae9473b5b2303f861e518b5ce52a5507452
bread <tiagu.1789@gmail.com>
2026-04-27 01:20:45 +0100

    Remove unused import from instrumentation.ts
    Removed unused import of initCronJobs from utils/cronJobs.

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,3 @@
-import { initCronJobs } from './utils/cronJobs';
-
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     const { closeActiveSessions } = await import('./utils/closesessions');
