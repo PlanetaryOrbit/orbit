a5281e88eafb7ef14ddf8e11f2a0d8352fffa613
buddywinte <buddywinte@gmail.com>
2026-07-06 16:39:50 -0500

    remove extra junk

---
+++ pages/api/workspace/[id]/forms/index.ts
@@ -0,0 +1,6 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '@/utils/database';
+
+export async function handler(req: NextApiRequest, res: NextApiResponse) {
+  
+}
