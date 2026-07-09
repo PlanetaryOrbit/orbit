ec4c72d5c3e3a77f7fb2255ae5ce220d113a1303
breadddevv <tiagu.1789@gmail.com>
2026-07-09 19:46:19 +0100

    Removed the rest of the imports

--- lib/security.ts
+++ lib/security.ts
@@ -1,58 +1,8 @@
 import type {
   NextApiHandler,
   NextApiRequest,
-  NextApiResponse,
-  GetServerSidePropsContext,
-  GetServerSidePropsResult,
+  NextApiResponse
 } from "next";
-
-import { getSessionByToken } from "@/utils/session";
-import zxcvbn from "zxcvbn";
-import * as cookie from "cookie";
-import * as crypto from "crypto";
-import { getConfig } from "@/utils/configEngine";
-import prisma from "@/utils/database";
-import { error } from "console";
-import { request } from "http";
-
-if (process.env.NODE_ENV === "production") {
-  const secret =
-    process.env.SESSION_SECRET || crypto.randomBytes(32).toString("hex");
-
-  if (secret === "supersecretpassword") {
-    throw new Error(
-      "SESSION_SECRET must be changed from the default secret in production",
-    );
-  }
-
-  const strength = zxcvbn(secret);
-
-  if (strength.score < 4) {
-    throw new Error(
-      `SESSION_SECRET is not strong enough. Score: ${strength.score}/4. Please generate a secret using "openssl rand -base64 32".`,
-    );
-  }
-}
-
-export type AuthHandler<T = any> = (
-  req: AuthenticatedRequest,
-  res: NextApiResponse<T>,
-) => unknown | Promise<unknown>;
-
-export interface AuthenticatedRequest extends NextApiRequest {
-  auth: {
-    userId: bigint;
-    token: string;
-    session: Awaited<ReturnType<typeof getSessionByToken>>;
-  };
-  session: {
-    userid: bigint;
-  };
-}
-
-export interface APIRequest extends NextApiRequest {
-  workspaceId?: bigint;
-}
 
 export function verifyWorkspace(req: NextApiRequest): boolean {
   const { id: workspaceId } = req.query;
